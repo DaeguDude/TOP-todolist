70b6f9ddaf3d9ba06ce0cf541c48945752fb9c7e
Daegudude <k3hppk@gmail.com>
2020-10-19 13:39:17 +0900

    Every files that are not part of HTMLDesign is deleted

--- modalLogic/modalLogic.js
+++
@@ -1,7 +0,0 @@
-let modal = document.querySelector('.modal');
-let categoryContainer = document.querySelector('div.category-container')
-window.onclick = (event) => {
-    if (event.taraget === categoryContainer) {
-        categoryContainer.remove();
-    }
-}
