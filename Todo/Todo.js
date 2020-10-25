4bbc2e37c156e890a4ca0bd1d8223989cd302ddf
Daegudude <k3hppk@gmail.com>
2020-10-25 16:38:43 +0900

    Working on making todo module

---
+++ Todo/Todo.js
@@ -0,0 +1,74 @@
+const Todo = (title, ...args) => {
+  let _title = title;
+  let _description = args[0];
+  let _dueDate = args[1];
+  let _createdDate = args[2];
+  let _priority = args[3];
+  let _category = args[4];
+
+  // ------------------------------------------
+  // SET
+  // ------------------------------------------
+  const setTitle = (title) => {
+    _title = title;
+  }
+
+  const setDescription = (description) => {
+    _description = description
+  }
+
+  const setDueDate = (dueDate) => {
+    _dueDate = dueDate;
+  }
+
+  const setCreatedDate = (createdDate) => {
+    _createdDate = createdDate;
+  }
+
+  const setPriority = (priority) => {
+    _priority = priority;
+  }
+
+  const setCategory = (category) => {
+    _category = category;
+  }
+
+  // ------------------------------------------
+  // GET
+  // ------------------------------------------
+  const getTitle = () => {
+    return _title;
+  }
+
+  const getDescription = () => {
+    return _description;
+  }
+
+  const getDueDate = () => {
+    return _dueDate;
+  }
+
+  const getCreatedDate = () => {
+    return _createdDate;
+  }
+
+  const getPriority = () => {
+    return _priority;
+  }
+
+  const getCategory = () => {
+    return _category;
+  }
+
+  
+  return {
+    // SET
+    setTitle, setDescription, setDueDate,
+    setCreatedDate, setPriority, setCategory,
+    getTitle, getDescription, getDueDate,
+    getCreatedDate, getPriority, getCategory
+  }
+}
+
+
+
